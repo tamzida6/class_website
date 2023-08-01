57ebf4a5609904580ac2fda323a1fff33e352457
tamzida6 <115437330+tamzida6@users.noreply.github.com>
2023-08-01 19:26:16 -0400

    Add files via upload
    class website

---
+++ lab3.js
@@ -0,0 +1,13 @@
+function display(){}
+    vaar a = new Date(2022);
+    document.getElementById("date").innerHTML = a;
+  }
+
+  function add(){
+    var a = 25;
+    var b = 15;
+    var c = 25;
+    var d = 12;
+    var z = a+b+c+d;
+    document.getElementById("a1").innerHTML = "$" + "" +z;
+  }
