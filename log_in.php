97b82457b1ccf5797c639f21e35576b082445aa9
tamzida6 <115437330+tamzida6@users.noreply.github.com>
2023-08-16 16:39:02 -0400

    Add files via upload

---
+++ log_in.php
@@ -0,0 +1,25 @@
+<?php
+$fnam = $_POST['fname'];
+$lnam = $_POST['lname'];
+$age = $_POST['age'];
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+
+$conn = mysqli_connect("localhost", "root", "root", "personal_info");
+$sql = "INSERT INTO `info`(`first_name`, `last_name`, `dob`, `email`, `pass`) VALUES ('$fnam','$lnam','$age','$email','$pass')";
+
+$res = mysqli_query($conn, $sql);
+
+if ($res){
+    //echo "Information stored successfully.";
+    header('Location: http://localhost:8888/project.html');
+    exit();
+ }
+
+    else{
+        echo "some error";
+    }
+
+$mysqli->close();
+
+    ?>
---
+++ pjs.js
@@ -0,0 +1,36 @@
+const heightInput = document.querySelector("#height-input");
+const weightInput = document.querySelector("#weight-input");
+const calculateButton = document.querySelector("#calculate-btn");
+const result = document.querySelector("#results");
+const statement = document.querySelector("#statement");
+let BMI = 0,
+  heightCm = 0,
+  weight = 0;
+
+calculateButton.addEventListener("click", () => {
+  heightCm = parseFloat(heightInput.value);
+  weight = parseFloat(weightInput.value);
+
+  const heightM = heightCm / 100;
+
+  BMI = weight / (heightM * heightM);
+
+  result.textContent = `Results: ${BMI.toFixed(2)}`;
+
+  if (BMI < 18.5) {
+    statement.textContent = "Your BMI falls within the underweight range";
+  } else if (BMI >= 18.5 && BMI < 24.9) {
+    statement.textContent =
+      "Your BMI falls within the normal or healthy weight range";
+  } else if (BMI >= 25 && BMI < 29.9) {
+    statement.textContent = "Your BMI falls within the overweight range";
+  } else {
+    statement.textContent = "Your BMI falls within the obese range";
+  }
+});
+
+function display() {
+  const a = new Date().toISOString().split("T")[0]; //to give this format 2023-07-28
+  document.getElementById("date").textContent = a;
+}
+
